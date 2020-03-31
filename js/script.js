40caed7746fd091a061b08eba1e32697d03e4059
pojoba23 <62113047+pojoba23@users.noreply.github.com>
2020-03-31 12:51:37 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,18 +5,10 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+const studentsList = document.querySelectorAll("li"); //targets all the li's on the page.
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
+const itemsPerPage = 10; //limits the amount of items that will appear on the page to 10.
    
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
 
 
 
@@ -34,6 +26,14 @@ FSJS project 2 - List Filter and Pagination
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
+//Display Page Function
+
+const showPage = (list, page) => {
+   console.log(listItems[2])
+
+}
+
+
 
 
 
@@ -46,5 +46,3 @@ FSJS project 2 - List Filter and Pagination
 
 
 
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
