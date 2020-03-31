ea85eee9231a8ecd0dfd7d077951c0d4dfad8b35
pojoba23 <62113047+pojoba23@users.noreply.github.com>
2020-03-31 13:26:39 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,28 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+Powell Barrett
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+
 
 const studentsList = document.querySelectorAll("li"); //targets all the li's on the page.
 
 const itemsPerPage = 10; //limits the amount of items that will appear on the page to 10.
    
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 //Display Page Function
 
 const showPage = (list, page) => {
-   console.log(listItems[2])
-
-}
+  let startIndex = (page * itemsPerPage) - itemsPerPage;
+  let endIndex = (page * itemsPerPage)
+  
+  for (let i=1; i<list.length; i+=1){
+    if (list[i] >= startIndex && list [i] <= endIndex)
+      list [i].style.display = '';
+  } else{
+    list[i].style.display = 'none';
+  }
+};
 
 
 
