ab54efd941f14bc59cf5c73f7aea44ebebbf56eb
pojoba23 <62113047+pojoba23@users.noreply.github.com>
2020-04-01 12:15:54 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,9 +30,34 @@ const showPage = (list, page) => {
    functionality to the pagination buttons.
 ***/
 const appendPageLinks = (list) => {
-  
-
+  const page_list = Math.ceil(list.length / students_per_page);
+    const page_div = document.querySelector('.page');
+    const new_div = document.createElement('div');
+    const ul = document.createElement('ul');
+    new_div.className = 'pagination';
+    page_div.appendChild(new_div);
+    new_div.appendChild(ul);
 }
+  for(let i = 0; i < page_list; i++){
+    let li = document.createElement('li');
+    let anchor = document.createElement('a');
+    anchor.setAttribute('href', '#');
+    anchor.textContent = i + 1;
+    li.appendChild(anchor);
+    ul.appendChild(li);
+    if(i === 0){
+      anchor.className = 'active';
+    }}
 
+    anchor.addEventListener('click', (e) => {
+      const li_active = document.querySelectorAll('.pagination a');
 
-
+      for(let j = 0; j < li_active.length; j++){
+         li_active[j].classList.remove('active');
+      } 
+      e.target.className = 'active';
+      show_page(student_list, e.target.textContent);
+      e.preventDefault();
+    });
+    showPage (studentsList, 1)
+    appendPageLinks (studentsList)
