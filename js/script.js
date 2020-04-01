28307cea3dc9e160147cc624dab1c97b4ecafc74
pojoba23 <62113047+pojoba23@users.noreply.github.com>
2020-04-01 10:52:43 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ FSJS project 2 - List Filter and Pagination
 Powell Barrett
 ******************************************/
 
-
+//Global Variables
 
 const studentsList = document.querySelectorAll("li"); //targets all the li's on the page.
 
@@ -12,18 +12,18 @@ const itemsPerPage = 10; //limits the amount of items that will appear on the pa
    
 //Display Page Function
 
-const showPage = (list, display) => {
-  list= itemsPerPage;
-  studentsPerPage = 10
-  pagesPerStudents = itemsPerPage / 10;
+const showPage = (list, page) => {
+  const startIndex= (page * itemsPerPage) - itemsPerPage;
+  const endIndex = (page * itemsPerPage) - 1;
   for (let i = 0; i < list.length; i++) {
-    if (i >= studentsPerPage) {
-      list[i].style.dipslay = "none";
+    if (i >= startIndex && i <= endIndex) {
+      list[i].style.dipslay = "";
+    } else {
+      list[i].style.diplay = 'none';
+
     }
   }
-  return showPage
 };
-console.log (showPage)
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
